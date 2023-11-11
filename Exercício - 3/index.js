b24d79268ed7db52eef16dd4efb857423f525e3d
Pedro Luis C Lopes <pedroluiscostalopes@hotmail.com>
2023-11-11 19:11:16 -0300

    Realizado em: 11/11/2023

---
+++ Exercício - 3/index.js
@@ -0,0 +1,32 @@
+// Exercício 03 --------
+//----------------------
+// Responda o questinoário abaixo usando os conhecimentos apresentados
+// na aula 24.
+/*----------------------
+A segunda letra do seu nome é: 
+Qual o primeiro índice da letra no seu nome? 
+Qual o último índice da letra no seu nome? 
+As últimas 3 letras do seu nome são: 
+As palavras do seu nome são:
+Seu nome com letras maiúsculas: 
+Seu nome com letras minúsculas: 
+------------------------*/
+
+// Cria um pop-up para armazenar o valor.
+const nome = prompt('Qual seu nome completo?');
+// Cria um espaço html com o valor atribuido.
+document.body.innerHTML += `Site simples sobre String <3<br /><br />`
+
+document.body.innerHTML += `Seu nome completo é ${nome}.<br />`
+document.body.innerHTML += `----------<br />`
+document.body.innerHTML += `A segunda letra do seu nome é: ${nome.charAt(0)} <br />`
+document.body.innerHTML += `----------<br />`
+document.body.innerHTML += `Qual o último índice da sua letra? ${nome.length - 1}<br />`
+document.body.innerHTML += `----------<br />`
+document.body.innerHTML += `As últimas 3 letras do seu nome: ${nome.slice(nome.length - 3)}<br />`
+document.body.innerHTML += `----------<br />`
+document.body.innerHTML += `As palavras do seu nome são: ${nome.split(' ').join(', ')}<br />`
+document.body.innerHTML += `----------<br />`
+document.body.innerHTML += `Seu nome em maiúsculo: ${nome.toUpperCase()}<br />`
+document.body.innerHTML += `----------<br />`
+document.body.innerHTML += `Seu nome em minúsculo: ${nome.toLowerCase()}<br />`
