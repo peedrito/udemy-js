52d0c1399141a2818fbaf85d2c50f9593e44e924
Pedro Luis C Lopes <pedroluiscostalopes@hotmail.com>
2023-11-10 22:13:22 -0300

    Realizado em: 10/11/2023

---
+++ Exercício - 2/index-01.js
@@ -0,0 +1,23 @@
+// Exercício 02 -------
+// Faça um script que armazene um valor através do prompt do navegador.
+//---------------------
+// O exercicio não aborda nenhum tema específico, por esse motivo, 
+// vou optar por fazer um script baseado no exercicio 01, porém agora no navegador. 
+//---------------------
+
+// Poderíamos aqui usar um objeto, mas vou seguir a didatica do curso.
+const nome = prompt('Digite seu nome, por favor!');
+const idade = prompt('Digite sua idade, por favor.');
+let altura = prompt('Digite sua altura em *metros*!');
+let peso = prompt('Digite seu peso em kg.')
+
+// Aqui vamos fazer o cáculo IMC da pessoa.
+// Mas antes precisamos transformar peso e altura em "number", pois está em "string".
+altura = Number(altura);
+peso = Number(peso);
+
+// Calculo:
+const IMC = (peso / (altura * altura));
+
+// Exibe em forma de pop-up o resultado.
+alert(`${nome} tem atualmente ${idade} anos, sua altura é de ${altura} em metros, seu peso atual é de ${altura}, seu IMC é de ${IMC.toFixed()}`)
